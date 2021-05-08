81b5a58ed2d5116fd73c98abb0ef3830e789f3db
jake chadwell <jake.chadwell01@gmail.com>
2021-05-08 16:11:15 -0500

    77

--- javascript-101-exercises.js
+++ javascript-101-exercises.js
@@ -1281,6 +1281,15 @@ addToDone("Exercise 76 is correct.")
 // Exercise 77
 // Write a function definition named onlyPositiveEvens that takes in sequence of numbers and returns an array containing all the positive evens from the sequence
 
+function onlyPositiveEvens(x){
+    let newArray = [];
+    for (let i = 0; i<x.length; i++){
+        if(x[i]>0 && x[i]%2===0){
+            newArray.push(x[i]);
+        }
+    } return newArray;
+}
+
 assert(onlyPositiveEvens([1, -2, 3]), [], "Exercise 77");
 assert(onlyPositiveEvens([2, -5, -6]), [2], "Exercise 77");
 assert(onlyPositiveEvens([3, 3, 4, 6]), [4, 6], "Exercise 77");
@@ -1290,6 +1299,8 @@ addToDone("Exercise 77 is correct.")
 
 // Exercise 78
 // Write a function definition named onlyPositiveOdds that takes in sequence of numbers and returns an array containing all the positive odd numbers from the sequence
+
+
 
 assert(onlyPositiveOdds([1, -2, 3]), [1, 3], "Exercise 78");
 assert(onlyPositiveOdds([2, -5, -6]), [], "Exercise 78");
